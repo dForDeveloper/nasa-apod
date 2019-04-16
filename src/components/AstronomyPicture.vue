af0e736835434dc543dd57901d81d61ede57020c
Jeo D <e8gea6w5z8id@opayq.com>
2019-04-15 23:06:04 -0600

    Render image in AstronomyPicture

--- src/components/AstronomyPicture.vue
+++ src/components/AstronomyPicture.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="astronomy-picture">
-    astronomy picture
+    <h2>astronomy picture</h2>
+    <p v-if=loading>Loading...</p>
+    <img
+      v-else
+      v-bind:src="pictures[day - 1].url"
+      v-bind:alt="pictures[day - 1].title"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AstronomyPicture'
+  name: 'AstronomyPicture',
+  props: ['pictures', 'loading', 'day']
 }
 </script>
 
