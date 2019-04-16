56beae2382a4bc4602f5a94cbd21d8af8274c386
Jeo D <e8gea6w5z8id@opayq.com>
2019-04-16 17:08:18 -0600

    Add NotFound component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="p">Page not found</p>
+</template>
+
+<script>
+  export default {
+    name: 'NotFound'
+  }
+</script>
+
+<style scoped>
+.p {
+  width: 100%;
+  text-align: center;
+  margin: 8px 0;
+}
+</style>
